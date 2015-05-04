83a04f5fe4515c1ea8180d6c0799c83c076e012e
Nick <nres@umich.edu>
2015-05-04 13:16:45 -0400

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
-module ApplicationHelper
-end
+module ApplicationHelper #This module declaration allows us to have the full_title function available in all our views
+    def full_title(page_title = '')
+      base_title = "Nick's First Application"
+      if page_title.empty?
+        base_title
+      else
+        "#{page_title} | #{base_title}"
+      end
+    end
+  end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Nick's First Application"
+  end
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Nick's First Application"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Nick's First Application"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Nick's First Application"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
