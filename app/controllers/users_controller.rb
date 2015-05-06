7965bbe5bea6f41ce6c3543ea7a2a05222fa4fa5
Nick <nres@umich.edu>
2015-05-06 14:03:21 -0400

    Finish edit/update/etc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :index] #specificying edit and update means that the before requirement only pertains to those methods
+  before_action :logged_in_user, only: [:edit, :update, :index, :destory] #specificying edit and update means that the before requirement only pertains to those methods
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   def show
     @user = User.find(params[:id])
   end
 
   def index #remember that when this, and edit and new are called upon by a route, it automatically goes to the view with the same name as well
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def new
@@ -53,10 +54,24 @@ class UsersController < ApplicationController
     redirect_to(root_url) unless current_user?(@user) #This implies current_user takes the value of a user object
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  def admin_user #Defines admin user for before action
+    if current_user && !current_user.admin?
+      redirect_to root_url
+    elsif !current_user
+      redirect_to login_path
+    end
+  end
+
   private #AAAAAAAAAAAAANNNNNNYYYTTTTHHHHHHHHIIIINNNNGGGGG Below here is PRIVATE
 
     def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :password, #Note here that remember_digest and admin are left out so ppl can't edit them
                                  :password_confirmation)
     end
 
