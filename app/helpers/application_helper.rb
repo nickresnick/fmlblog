b1b5d96df2824cfa5d72860513c6ef2299bd8f19
nickresnick <nres@umich.edu>
2015-10-19 22:27:12 -0400

    fix the page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper #This module declaration allows us to have the full_title function available in all our views
     def full_title(page_title = '')
-      base_title = "Nick's First Application"
+      base_title = "Nick Resnick's Blog"
       if page_title.empty?
         base_title
       else
