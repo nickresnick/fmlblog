cd4e24a7fd306ddca602401189adc8fca8804034
nickresnick <nres@umich.edu>
2015-10-22 11:05:18 -0400

    Post model, deleted attr_accessor

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
-  attr_accessor :content, :title, :name
 
   validates :title, presence: true
   validates :name, presence: true
   validates :content, length: {minimum: 5}
+
 end
