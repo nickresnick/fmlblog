5117b4002719d409808bbf6b2ee1d8ec283de605
nickresnick <nres@umich.edu>
2015-10-22 11:26:36 -0400

    added create method for comments, changed show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,7 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(post_params)
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
 end
