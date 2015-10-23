68c449a9c5912b5af0e6da3e982bd0e3ae71805d
nickresnick <nres@umich.edu>
2015-10-23 16:41:07 -0400

    changing the logged in home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # Users
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Nick",
+             email: "nres@umich.edu",
+             password:              "DEN11.23",
+             password_confirmation: "DEN11.23",
              admin:     true,
              activated: true,
              activated_at: Time.zone.now)
