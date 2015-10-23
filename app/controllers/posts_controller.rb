d784c2f5ca23ecd91f998b8e17b6f92997b006ef
nickresnick <nres@umich.edu>
2015-10-23 15:09:20 -0400

    deleted sign up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:create, :destroy]
-  before_action :correct_user, only: :destroy
+  before_action :logged_in_user, only: [:create, :destroy, :edit]
+  before_action :correct_user, only: [:destroy, :edit]
 
   # GET /posts
   # GET /posts.json
