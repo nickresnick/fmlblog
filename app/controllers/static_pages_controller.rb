ccc22c725b385f4ee9b223ed9847e55504ccc106
nickresnick <nres@umich.edu>
2015-10-23 16:48:33 -0400

    maybe fixed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
-
+      @posts = Post.includes(:comments).order("created_at desc").limit(5)
     else
     @posts = Post.includes(:comments).order("created_at desc").limit(5)
     end
