4773b8e367b04cd4b1a460194d23702dbd460581
nickresnick <nres@umich.edu>
2015-10-23 23:54:36 -0400

    changing around the AWS keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,12 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
     config.root = Rails.root.join('tmp') # adding these...
     config.cache_dir = 'carrierwave' # ...two lines
 
     config.fog_credentials = {
         # Configuration for Amazon S3
         :provider              => 'AWS',
-        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY']
+        :aws_access_key_id     => ENV['S3_KEY'],
+        :aws_secret_access_key => ENV['S3_SECRET']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
-  end
 end
