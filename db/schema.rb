7228a501b55c80ab54e51445a1ad8ecd7729e162
nickresnick <nres@umich.edu>
2015-10-23 20:27:48 -0400

    adding pictures to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023131837) do
+ActiveRecord::Schema.define(version: 20151024001942) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20151023131837) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
   end
 
   create_table "relationships", force: :cascade do |t|
