f0d7f6f011a43e3caf18b0e390be7e667a3c4057
nickresnick <nres@umich.edu>
2015-10-23 22:25:27 -0400

    at this point, everything is functional, next need to figure out how to have user specific about page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,10 @@ User.create!(name:  "Nick",
              activated: true,
              activated_at: Time.zone.now)
 
-
+User.create!(name:  "Sam",
+             email: "samcorey@umich.edu",
+             password:              "baebabae",
+             password_confirmation: "baebabae",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
