33bed63695378289e40bf9482b49fd2364ffd65c
nickresnick <nres@umich.edu>
2015-10-23 20:57:59 -0400

    db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -53,15 +53,11 @@ ActiveRecord::Schema.define(version: 20151024001942) do
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.string   "title"
-    t.integer  "user_id"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "picture"
   end
-
-  add_index "posts", ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
