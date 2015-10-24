e0511c59390e1179c1c71f1e299c2589a793e6ae
nickresnick <nres@umich.edu>
2015-10-23 20:27:14 -0400

    adding pictures to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,21 @@
 class Post < ActiveRecord::Base
 
+  has_many :comments
+  belongs_to :user
+
+  mount_uploader :picture, PictureUploader
+
   validates :title, presence: true
   validates :name, presence: true
   validates :content, length: {minimum: 5}
+  validates :picture_size
 
-  has_many :comments
-  belongs_to :user
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 
 end
