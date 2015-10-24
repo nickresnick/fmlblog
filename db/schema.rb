cdafda32a379c7cb3486291eeba984da4173cec3
nickresnick <nres@umich.edu>
2015-10-23 20:53:02 -0400

    changed db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20151024001942) do
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.string   "title"
+    t.integer  "user_id"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
