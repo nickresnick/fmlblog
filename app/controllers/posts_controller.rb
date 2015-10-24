c1e9c08ea80135a7b8f92f737b81b98144af61ac
nickresnick <nres@umich.edu>
2015-10-23 20:49:56 -0400

    changed create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,7 +40,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     if current_user.admin?
-      @post = Post.new(post_params)
+      @post = current_user.posts.build(post_params)
     else
       format.html { redirect_to root, notice: 'You are not authorized to make posts' }
     end
