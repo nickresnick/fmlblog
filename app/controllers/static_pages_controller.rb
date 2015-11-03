930fe93daeac2abb3d6fe416e5b65833b8e518b3
nickresnick <nres@umich.edu>
2015-11-03 15:11:28 -0500

    small change

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
-      @posts = Post.includes(:comments).order("created_at desc").limit(5)
+      @posts = Post.includes([:comments,:topic]).order("created_at desc").limit(5)
     else
-    @posts = Post.includes(:comments).order("created_at desc").limit(5)
+    @posts = Post.includes([:comments,:topic]).order("created_at desc").limit(5)
     end
 end
 
