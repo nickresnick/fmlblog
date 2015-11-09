c76ae038fc2a2ef118865da444a778d4dfb0b411
nickresnick <nres@umich.edu>
2015-11-08 21:03:49 -0500

    do i even need the Topic in create?

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
 
-  attr_accessor :topic_id
   accepts_nested_attributes_for :topic, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy => true
 
   mount_uploader :picture, PictureUploader
