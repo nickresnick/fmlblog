db55e64e5dcce28f2fb7c17e8acfbef737683709
nickresnick <nres@umich.edu>
2015-11-09 12:00:29 -0500

    feed on front page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.paginate(page: params[:page])
+    @posts = @user.posts.order("created_at desc")
   end
 
   def index #remember that when this, and edit and new are called upon by a route, it automatically goes to the view with the same name as well
