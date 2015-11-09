9055ff87c0bc73544ddb65a7acd0b4bd18f7362a
nickresnick <nres@umich.edu>
2015-11-08 23:38:46 -0500

    get topic_id in there

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -93,6 +93,7 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :title, :content, :picture, :user_id)
+      params.require(:post).permit(:name, :title, :content, :picture, :user_id,
+      topic_attributes: [:id, :name])
     end
 end
