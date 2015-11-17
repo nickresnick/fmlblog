b34257d379b645fd149dd08d1fdabcfd9135d943
nickresnick <nres@umich.edu>
2015-11-17 10:41:55 -0500

    ransack gem installation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,6 @@ module ApplicationHelper #This module declaration allows us to have the full_tit
     end
 
   def options_for_topic
-    [1,2,3]
+    [['film',1],['music',2],['life',3]]
   end
   end
