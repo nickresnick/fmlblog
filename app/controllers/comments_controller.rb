b3805b648949f212faa0b24a34e4afc739d537e7
nickresnick <nres@umich.edu>
2015-11-21 14:17:33 -0500

    the soundcloud issue is just that it can't be the first 30 characters of the post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,9 @@
 class CommentsController < ApplicationController
+
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @post, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.comments.errors, status: :unprocessable_entity }
-      end
-    end
+    @post.comments.create(params[:comment].permit(:commenter, :body))
+    redirect_to post_path(@post)
   end
 
   def destroy
