546276cb06766b10d85f6f27ac704bdfc6100fef
nickresnick <nres@umich.edu>
2015-11-24 10:57:46 -0500

    more relationship deletions

--- db/schema.rb
+++ db/schema.rb
@@ -78,17 +78,6 @@ ActiveRecord::Schema.define(version: 20151119220814) do
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
-  create_table "relationships", force: :cascade do |t|
-    t.integer  "follower_id"
-    t.integer  "followed_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
-  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
-
   create_table "topics", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "name"
