0b28621be07dbb5e9e844344f6d0aa7fb25525dc
nickresnick <nres@umich.edu>
2015-11-24 10:48:31 -0500

    deleting micropost from tests

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -13,9 +13,9 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
-    assert_match @user.microposts.count.to_s, response.body
-    @user.microposts.paginate(page: 1).each do |micropost|
-      assert_match micropost.content, response.body
+    assert_match @user.posts.count.to_s, response.body
+    @user.posts.paginate(page: 1).each do |post|
+      assert_match post.content, response.body
       assert_select 'div.pagination'
     end
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -62,10 +62,10 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, '')
   end
 
-  test "associated microposts should be destroyed" do
+  test "associated posts should be destroyed" do
     @user.save
-    @user.microposts.create!(content: "Lorem ipsum")
-    assert_difference 'Micropost.count', -1 do #i.e. assert a difference *when* you destroy
+    @user.posts.create!(content: "Lorem ipsum")
+    assert_difference 'Post.count', -1 do #i.e. assert a difference *when* you destroy
       @user.destroy
     end
   end
@@ -87,15 +87,15 @@ class UserTest < ActiveSupport::TestCase
     lana    = users(:lana)
     michael.follow(lana)
     # Posts from followed user
-    lana.microposts.each do |post_following|
+    lana.posts.each do |post_following|
       assert michael.feed.include?(post_following)
     end
     # Posts from self
-    michael.microposts.each do |post_self|
+    michael.posts.each do |post_self|
       assert michael.feed.include?(post_self)
     end
     # Posts from unfollowed user
-    archer.microposts.each do |post_unfollowed|
+    archer.posts.each do |post_unfollowed|
       assert_not michael.feed.include?(post_unfollowed)
     end
   end
