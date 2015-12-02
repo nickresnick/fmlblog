1d724c4e0486ab01933816d85b97263cccaa338a
nickresnick <nres@umich.edu>
2015-12-02 12:50:17 -0500

    changing title of site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper #This module declaration allows us to have the full_title function available in all our views
     def full_title(page_title = '')
-      base_title = "FML Blog"
+      base_title = "Film, Music, & Life Blog"
       if page_title.empty?
         base_title
       else
