4ac725c64199a4b360c87029d59ce7de9af8b1b8
nickresnick <nres@umich.edu>
2015-12-02 11:55:57 -0500

    needed to run the db migrate to heroku duh

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,15 +10,14 @@ class PostsController < ApplicationController
   # GET /posts.json
 
   def index
-    @posts = Post.friendly.search(params[:search])
+    @posts = Post.search(params[:search])
   end
 
 
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @post = Post.friendly.find(params[:id])
-    @recent_posts = Post.friendly.includes([:comments,:topic]).order("created_at desc").limit(10)
+    @recent_posts = Post.includes([:comments,:topic]).order("created_at desc").limit(10)
   end
 
   def save_my_previous_url
