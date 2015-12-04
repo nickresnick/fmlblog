883e5c13f7fe68521ef239209b9f4e1fb496c74b
nickresnick <nres@umich.edu>
2015-12-04 17:58:48 -0500

    chaning header

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :logged_in_user, only: [:create, :destroy, :edit]
   before_action :correct_user, only: [:destroy]
+  before_action :admin_user, only: [:destroy]
   after_filter "save_my_previous_url", only: [:new]
 
   # GET /posts
@@ -89,6 +90,7 @@ class PostsController < ApplicationController
     @post = current_user.posts.find_by(id: params[:id])
     redirect_to root_url if @post.nil?
   end
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
