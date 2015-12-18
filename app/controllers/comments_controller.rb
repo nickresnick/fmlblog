fcd016e96ba75855158034c71720b8262161313b
nickresnick <nres@umich.edu>
2015-12-17 21:44:33 -0500

    paginate

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,28 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.friendly.find(params[:post_id])
-    @post.comments.create(params[:comment].permit(:commenter, :body))
-    @comment = @post.comments.find(params[:id])
-    @comment.comment_submission
-    redirect_to post_path(@post)
+    @comment = @post.comments.create(comment_params)
+    if @comment.save
+      @comment.comment_submission
+      redirect_to @post
+    else
+      flash.now[:danger] = "error"
+    end
   end
+
+
 
   def destroy
     @post = Post.friendly.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :post_id)
   end
 
 end
