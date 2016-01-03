fdc9f50a1df7bbd07967b1176cf6cd9e29a8fd8f
nickresnick <nres@umich.edu>
2016-01-03 13:42:09 -0500

    paginate again

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def home
-    @posts = Post.paginate(:page => params[:page], :per_page => 30).order("created_at desc")
+    @posts = Post.paginate(:page => params[:page], :per_page => 10).order("created_at desc")
     @recent_posts = Post.includes([:comments,:topic]).order("created_at desc").limit(10)
 end
 
