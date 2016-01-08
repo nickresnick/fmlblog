4d5f08d10e900aa6726daa804b0ae6a13a3a2c99
nickresnick <nres@umich.edu>
2016-01-08 15:30:49 -0500

    meta viewport on posts show

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,10 @@ class StaticPagesController < ApplicationController
   def home
     @posts = Post.paginate(:page => params[:page], :per_page => 10).order("created_at desc")
     @recent_posts = Post.includes([:comments,:topic]).order("created_at desc").limit(10)
+
+    set_meta_tags :og => {
+                      :image    => '/kanye.jpg'
+                  }
 end
 
 
